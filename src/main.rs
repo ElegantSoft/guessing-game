83f58d19d04b3d3076c4d372733904a04103df9b
ElegantSoft= <react.laravel.dev@gmail.com>
2020-05-27 22:25:16 +0200

    finish guessing game

---
+++ src/main.rs
@@ -0,0 +1,36 @@
+use rand::Rng;
+use std::cmp::Ordering;
+use std::io;
+
+fn main() {
+    println!("guess the number!");
+    let secret_number = rand::thread_rng().gen_range(1, 101);
+
+    // println!("secret number is {}", secret_number);
+    loop {
+        let mut guess = String::new();
+
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to readLine");
+        println!("you guessed: {}", guess);
+
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => {
+                println!("please type correct number:");
+                continue;
+            }
+        };
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too High!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
+        }
+        println!("Please input your guess:");
+    }
+}
